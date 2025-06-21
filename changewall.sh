6af71203b130c0cc7b25d62b110c2a21aef0fa1b
GJaFreitas <gjacome-@student.42lisboa.com>
2025-06-22 00:58:19 +0100

    meh

--- changewall.sh
+++ changewall.sh
@@ -11,5 +11,5 @@ done
 chosen=$(echo "$filenames" | dmenu -l 10 -sf green -p "Wallpaper" -fn Terminus:size10)
 chosen=$(echo "$files" | grep $chosen)
 
+hellwal -i $chosen -m -b 0.1 &
 swww img $chosen --transition-step 20 --transition-fps 60 --transition-type center
-hellwal -i $chosen -m -b 0.1
