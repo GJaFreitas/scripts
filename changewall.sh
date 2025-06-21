8082aa0d4e75e1ab20a1e4ea6c8a85853787a7fe
GJaFreitas <gjacome-@student.42lisboa.com>
2025-06-21 16:02:55 +0100

    New wall script

---
+++ changewall.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+files=$(find ~/wall -type f)
+
+for file in $files
+do 
+	filenames+=$(basename $file)
+	filenames+=$'\n'
+done
+
+chosen=$(echo "$filenames" | dmenu -l 10 -sf green -p "Wallpaper" -fn Terminus:size10)
+chosen=$(echo "$files" | grep $chosen)
+
+swww img $chosen --transition-step 20 --transition-fps 60 --transition-type center
+hellwal -i $chosen -m -b 0.1
