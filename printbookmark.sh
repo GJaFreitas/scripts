92f67ea3050bdbe2e17933b1a4a2b89e481574bb
GJaFreitas <gjacome-@student.42lisboa.com>
2025-06-23 18:17:25 +0100

    idK

--- printbookmark.sh
+++ printbookmark.sh
@@ -6,4 +6,8 @@ to_type=$(grep -v '^#' $save_file | dmenu -i -l 25 -fn Terminus:size10| cut -d '
 #If nothing was chosen
 [[ -z $to_type ]] && exit
 
+# If the current active window is a zen browser instance then open
+# a new tab with the bookmark as a URL
+[[ -n $(hyprctl activewindow | grep Zen) ]] && zen-browser --new-tab $to_type && return
+
 wtype "$to_type"
