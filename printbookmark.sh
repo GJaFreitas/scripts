635530907e521a4df01c89372be059c762744c25
GJaFreitas <gjacome-@student.42lisboa.com>
2025-06-23 01:29:07 +0100

    More correction

--- printbookmark.sh
+++ printbookmark.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 save_file="$HOME/.local/share/bookmarks.txt"
-to_type=$(grep -v '^#' $save_file | dmenu -i -l 25 | cut -d ' ' -f 1)
+to_type=$(grep -v '^#' $save_file | dmenu -i -l 25 -fn Terminus:size10| cut -d ' ' -f 1)
 
 #If nothing was chosen
 [[ -z $to_type ]] && exit
--- scriptmenu.sh
+++ scriptmenu.sh
@@ -5,4 +5,4 @@ scripts=$(ls ~/scripts/ | grep -v addbookmark)
 run=$(echo "$scripts" | dmenu -l 10 -i -sf green -p "Script to run: " -fn Terminus:size10)
 
 # If some script was chosen then run it
-[[ -n $run ]] && sh $run
+[[ -n $run ]] && (sh ~/scripts/$run)
